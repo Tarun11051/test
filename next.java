b932fb261d7006588616872bc4a4175c23334881
Tarun11051 <tarunravi289@gmail.com>
2025-03-17 15:16:46 +0530

    forth commit

--- next.java
+++ next.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-import java.util.*;
+import java.util;
 public class Circle{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the area");
-		int a=sc.nextInt();
+	        double a=sc.nextInt();
 		System.out.println("enter the radius");
 		int r=sc.nextInt();
 		a=3.14*r*r;
-		scanner.close();
+		sc.close();
 	}
 }
 
