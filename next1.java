e421b5b6836bf1e276b271243c63683f6ec975c2
Tarun11051 <tarunravi289@gmail.com>
2025-03-17 15:27:39 +0530

    first commit

---
+++ next1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class next11111111111{
+        public static void main(String[]args){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("enter the radius");
+                double r=sc.nextInt();
+                a=3.14*r*r;
+                System.out.println(a);
+                sc.close();
+        }
+}
+
