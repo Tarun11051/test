165abc9f703c8c6f1ae84ff271cd6e2491ff8ecc
Tarun11051 <tarunravi289@gmail.com>
2025-03-17 15:18:00 +0530

    fifth commit

--- next.java
+++ next.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-import java.util;
-public class Circle{
+public class next{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the area");
